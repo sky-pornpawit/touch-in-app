16a46616929d52199cb753fbe7ea8d0a595b964b
Phitchaporn P <phitchaporn.p@touchtechnologies.co.th>
2020-04-13 16:11:52 +0700

    log project id from circleci env

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import logo from './logo.svg'
 import './App.css'
 
 function App() {
+  console.log(process.env.REACT_APP_PROJECT_ID)
   return (
     <div className='App'>
       <header className='App-header'>
