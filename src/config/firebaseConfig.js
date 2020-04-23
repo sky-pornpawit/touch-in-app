a4b0243647c6d29b29c7bada2d4e4ccaf9bd45fc
Phitchaporn P <phitchaporn.p@touchtechnologies.co.th>
2020-04-23 10:04:31 +0700

    update firbase config

--- src/config/firebaseConfig.js
+++ src/config/firebaseConfig.js
@@ -6,15 +6,26 @@ import 'firebase/storage'
 import 'firebase/database'
 import 'firebase/messaging'
 
+// const config = {
+//   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+//   authDomain: `${process.env.REACT_APP_FIREBASE_PROJECT_ID}.firebaseapp.com`,
+//   databaseURL: `https://${process.env.REACT_APP_FIREBASE_PROJECT_ID}.firebaseio.com`,
+//   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+//   storageBucket: `${process.env.REACT_APP_FIREBASE_PROJECT_ID}.appspot.com`,
+//   messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_ID,
+//   appId: process.env.REACT_APP_FIREBASE_APP_ID,
+//   measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
+// }
+
 const config = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: `${process.env.REACT_APP_FIREBASE_PROJECT_ID}.firebaseapp.com`,
-  databaseURL: `https://${process.env.REACT_APP_FIREBASE_PROJECT_ID}.firebaseio.com`,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: `${process.env.REACT_APP_FIREBASE_PROJECT_ID}.appspot.com`,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
-  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
+  apiKey: 'AIzaSyBBajrAIcxdN8Z0pT3U6Jl-kJBI4NNGE7c',
+  authDomain: 'touch-in-app.firebaseapp.com',
+  databaseURL: 'https://touch-in-app.firebaseio.com',
+  projectId: 'touch-in-app',
+  storageBucket: 'touch-in-app.appspot.com',
+  messagingSenderId: '910531456139',
+  appId: '1:910531456139:web:4114f0942937715c4e788e',
+  measurementId: 'G-TXT2Z9G76G',
 }
 
 firebase.initializeApp(config)
